01471792b1c9011a14527817bdfa6abeea486852
Mira090 <2935shu4182@gmail.com>
2026-08-31 18:25:12 +0900

    光無き刃の攻撃時の移動を0%に変更

--- SephiriaArcaneForged/ArcaneWeapons/ArcaneWeapon_EnhancedDashAttack.cs
+++ SephiriaArcaneForged/ArcaneWeapons/ArcaneWeapon_EnhancedDashAttack.cs
@@ -21,6 +21,7 @@ namespace SephiriaArcaneForged.ArcaneWeapons
         public override NewWeaponFireData FireData => SephiriaPrefabs.DashAttackFireData_BladeZone;
         public override string DamageId => "ArcaneWeapon_EnhancedDashAttack";
         public override float? DamageMultiplier => null;
+        public override float AttackDashScale => 0f;
         private void OnDashServerside(Vector2 motionTo, bool consumed)
         {
             if (consumed)
